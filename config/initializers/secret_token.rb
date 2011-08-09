cdd275ce9836656bb7b9b5086358563769e6de0b
Matthias Folz <folz@avarteq.de>
2011-08-09 19:38:04 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Miner::Application.config.secret_token = 'a37ccc33398ebd209926a4edc150c276ecfc454ef67f2ad7ef11d9607c7fc5bf045183d8a426e7e50f5722b61e4a3db04cd74dab102084d578bd1184b7706b2d'
